b0e72aead9b24f63f1f607756b78222c9ce085b4
JoeShane11 <joeshane1103@gmail.com>
2022-10-24 16:07:10 +0700

    form category fix

--- uts/kelompok_1_uts/routes/movie_category.py
+++ uts/kelompok_1_uts/routes/movie_category.py
@@ -4,13 +4,13 @@ from kelompok_1_uts.forms.movie_category import CategoryForm
 from kelompok_1_uts.models.movie_category import MovieCategory
 from kelompok_1_uts.controllers import movie as category_controller
 
-bp = Blueprint("category", __name__, template_folder="templates", static_folder="static")
+bp = Blueprint("movie_category", __name__, template_folder="templates", static_folder="static")
 
 @bp.route("/")
 def index():
     
     data = category_controller.get_all()
-    return render_template("category/list.html", data=data)
+    return render_template("movie_category/list.html", data=data)
     
 
 @bp.route("/create", methods=["GET", "POST"])
@@ -24,9 +24,9 @@ def create():
         )
 
         flash("Data staf berhasil ditambahkan.", category="success")
-        return redirect(url_for("category.index"))
+        return redirect(url_for("movie_category.index"))
 
-    return render_template("category/form.html", form=form, data=None)
+    return render_template("movie_category/form.html", form=form, data=None)
 
 
 @bp.route("/update/<int:id>", methods=["POST"])
@@ -39,7 +39,7 @@ def update(id):
     )
 
     flash("Data kategori berhasil diubah.", category="primary")
-    return redirect(url_for("category.index"))
+    return redirect(url_for("movie_category.index"))
 
 @bp.route("/show")
 def show():
@@ -51,4 +51,4 @@ def delete():
     category_controller.delete(request.form.get("id"))
 
     flash("Data kategori berhasil dihapus.", category="danger")
-    return redirect(url_for("category.index"))
+    return redirect(url_for("movie_category.index"))
