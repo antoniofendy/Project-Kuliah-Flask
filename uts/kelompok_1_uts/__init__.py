8631894ff4f7f793e90fbc4958161a5d57b02c2b
JoeShane11 <joeshane1103@gmail.com>
2022-10-24 07:39:01 +0700

    Redirect Movie Route to single file and register it

--- uts/kelompok_1_uts/__init__.py
+++ uts/kelompok_1_uts/__init__.py
@@ -21,6 +21,7 @@ def create_app():
     from kelompok_1_uts.routes.stock import bp as stock_bp
     from kelompok_1_uts.routes.member import bp as member_bp
     from kelompok_1_uts.routes.charge_rule import bp as charge_rule_bp
+    from kelompok_1_uts.routes.movie import bp as movie_bp
     from kelompok_1_uts.errors import bp as errors_bp
 
     app.register_blueprint(main_bp)
@@ -28,6 +29,7 @@ def create_app():
     app.register_blueprint(stock_bp, url_prefix="/stock")
     app.register_blueprint(member_bp, url_prefix="/member")
     app.register_blueprint(charge_rule_bp, url_prefix="/charge-rule")
+    app.register_blueprint(movie_bp)
     app.register_blueprint(errors_bp)
 
     return app
