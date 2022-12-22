50c85e7682116fe40e82271decc7f4e2af2a3fc2
antoniofendy <layfenfen@gmail.com>
2022-12-22 09:51:30 +0700

    Login and Register Page

--- uas/kelompok_1_uas/user/routes/user.py
+++ uas/kelompok_1_uas/user/routes/user.py
@@ -1,13 +1,17 @@
 from kelompok_1_uas.user.models.user import User
 from kelompok_1_uas.user.forms.user import UserForm
+from kelompok_1_uas import db
 from kelompok_1_uas.user.controllers import user as user_controller
+from sqlalchemy import exc
+
+import hashlib
 
 from flask import Blueprint, render_template, request, flash, url_for, redirect
 
 user_user_bp = Blueprint(
-    "user_user",
+    "user",
     __name__,
-    url_prefix="/user/user",
+    url_prefix="/user",
     template_folder="../templates",
 )
 
@@ -23,56 +27,55 @@ def read(id):
     return render_template("user/user/list.html", data=user_controller.get_all())
 
 
-@user_user_bp.route("/create", methods=["GET", "POST"])
-def create():
+@user_user_bp.route("/register", methods=["GET", "POST"])
+def register():
     if request.method == "POST":
-        user_controller.create(
-            User(
-                name=request.form.get("nameUser"),
-                phone=request.form.get("phoneUser"),
-                address=request.form.get("addressUser"),
-                date_of_birth=request.form.get("date_of_birthUser"),
-                sex=request.form.get("sexUser"),
-                occupation=request.form.get("occupationUser"),
-                email=request.form.get("emailUser"),
-                password=request.form.get("passwordUser"),
-            )
-        )
 
-        flash("Data user baru berhasil ditambahkan.", category="success")
-        return redirect(url_for("user_user.read"))
+        md5_password = hashlib.md5(request.form.get("password").encode())
+        md5_password_confirm = hashlib.md5(request.form.get("password_confirm").encode())
 
-    return render_template("user/user/form.html", form=UserForm(), data=None)
+        if md5_password.hexdigest() == md5_password_confirm.hexdigest():
+            try:
+                user_controller.create(
+                    User(
+                        name=request.form.get("name"),
+                        phone=request.form.get("phone"),
+                        address=request.form.get("address"),
+                        date_of_birth=request.form.get("date_of_birth"),
+                        sex=request.form.get("sex"),
+                        occupation=request.form.get("occupation"),
+                        email=request.form.get("email"),
+                        password=md5_password.hexdigest(),
+                    )
+                )
+                flash("Berhasil registrasi, silahkan login.", category="success")
+                return redirect(url_for("user.login"))
 
-@user_user_bp.route("/update", methods=["POST"])
-def update():
-    data = {
-        "id": request.form.get("id"),
-        "nameUser": request.form.get("nameUser"),
-        "phoneUser": request.form.get("phoneUser"),
-        "date_of_birthUser": request.form.get("date_of_birthUser"),
-        "sexUser": request.form.get("sexUser"),
-        "occupationUser": request.form.get("occupationUser"),
-        "emailUser": request.form.get("emailUser"),
-        "addressUser": request.form.get("addressUser"),
-        "passwordUser": request.form.get("passwordUser"),
-    }
+            except exc.SQLAlchemyError:
+                flash("Terjadi kesalahan sistem.", category="danger")
+                return redirect(url_for("user.register"))
 
-    user_controller.update(data)
+        flash("Password dan password konfirmasi tidak sama.", category="danger")
+        return redirect(url_for("user.register"))
 
-    flash("Data user berhasil diubah.", category="primary")
-    return redirect(url_for("user_user.read"))
+    return render_template("site/registerUser.html")
 
 
 @user_user_bp.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        md5_password = hashlib.md5(password.encode())
+
+        user = db.session.query(User).filter_by(email=email, password=md5_password.hexdigest()).first()
+
+        # cek kredensial user
+        if not user:
+            flash('Email atau password tidak sesuai.', category="warning")
+            return redirect(url_for('user.login'))
+
+        # jika user terdaftar
+        return redirect(url_for("user_main.index"))
+
     return render_template("site/loginUser.html")
-
-
-@user_user_bp.route("/delete", methods=["POST"])
-def delete():
-    id_ = request.form.get("id")
-    user_controller.delete(id_)
-    
-    flash("Data user berhasil dihapus", category="info")
-    return redirect(url_for("user_user.read"))
