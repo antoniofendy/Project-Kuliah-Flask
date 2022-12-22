e0155b3539e2599c64362cb9a188647397120304
antoniofendy <layfenfen@gmail.com>
2022-12-23 06:01:22 +0700

    Laporan

--- uas/kelompok_1_uas/admin/routes/reporting.py
+++ uas/kelompok_1_uas/admin/routes/reporting.py
@@ -29,8 +29,19 @@ admin_reporting_bp = Blueprint(
 def generate_report():
     if request.method == "POST":
 
-        reservation = Reservation.query.all()
-        rendered = render_template("admin/report/pdf_template.html", reservation=reservation)
+        rent = Rent.query.where(Rent.updated_at >= request.form.get("start_date")).where(Rent.updated_at <= request.form.get("end_date")).all()
+        total = (db.session.query(Rent)
+        .where(Rent.updated_at >= request.form.get("start_date"))
+        .where(Rent.updated_at <= request.form.get("end_date"))
+        .with_entities(db.func.sum(Rent.total))
+        .first()[0])
+
+        # print(total)
+
+        start_date = request.form.get("start_date")
+        end_date = request.form.get("end_date")
+        
+        rendered = render_template("admin/report/pdf_template.html", rent=rent, total=total, start_date=start_date, end_date=end_date)
         config = pdfkit.configuration(wkhtmltopdf = 'C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe')
         pdf = pdfkit.from_string(rendered, configuration=config)
         
@@ -38,7 +49,7 @@ def generate_report():
         response.headers['Content-Type'] = 'template'
         response.headers['Content-Disposition'] = 'output.pdf'
         
-        print(response)
+        # print(response)
     
         return response
 
